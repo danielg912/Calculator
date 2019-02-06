a19c3caf28654bff152256e91911e2ba395a7dd0
Daniel Gonzalez <dgonzalez7752@gmail.com>
2019-02-06 13:12:05 -0600

    Finished CalculatorAdd test and created CalculatorSubtract Test

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -6,16 +6,32 @@ namespace Calculator.Tests
     public class UnitTest1
     {
         [Theory]
-        [InlineData(1, 1)]
-        public void CalculatorAdds(int number1, int number2)
+        [InlineData(1, 1, 2)]
+        public void CalculatorAdds(int number1, int number2, int expected)
         {
             // Arrange
             Calculator calculator = new Calculator();
 
 
             // Act
+            int actual = calculator.Add(number1, number2);
 
             // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        public void CalculatorSubtracts(int number1, int number2, int expected)
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int actual = calculator.Subtract(number1, number2);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
