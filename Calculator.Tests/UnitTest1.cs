c206cbd41ad08a829f3233cd9eafd81cec1ef79f
Daniel Gonzalez <dgonzalez7752@gmail.com>
2019-02-06 13:47:27 -0600

    Added CaclulatorTakesExponent Test

--- Calculator.Tests/UnitTest1.cs
+++ Calculator.Tests/UnitTest1.cs
@@ -21,7 +21,7 @@ namespace Calculator.Tests
         }
 
         [Theory]
-        [InlineData(2, 2, 4)]
+        [InlineData(2, 2, 0)]
         public void CalculatorSubtracts(int number1, int number2, int expected)
         {
             // Arrange
@@ -60,6 +60,21 @@ namespace Calculator.Tests
 
             // Assert
             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(5, 25)]
+        public void CalculatorTakesExponent(int number1, int expected)
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int actual = calculator.Exponents(number1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+
         }
     }
 }
